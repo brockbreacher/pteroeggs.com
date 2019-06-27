7c6ef6c45562e8557aa268336fc5330aef873608
Matthew Hatcher <matthewh@halfpetal.com>
2019-06-27 04:04:17 -0400

    Adding the egg listing.

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EggDataService } from '../services/egg-data.service';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
-
-  constructor() { }
+  constructor(private eggService: EggDataService) { }
 
   ngOnInit() {
   }
-
 }
--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -2,11 +2,14 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EggCardComponent } from '../egg-card/egg-card.component';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, EggCardComponent],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     RouterModule.forChild([{ path: '', component: IndexComponent }])
   ]
 })
