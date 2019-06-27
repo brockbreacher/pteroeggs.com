70ef6f6f5a7d184c868c58d52ce7d1a179872719
Matthew Hatcher <matthewh@halfpetal.com>
2019-06-27 04:03:38 -0400

    More egg card stuff.

---
+++ src/app/egg-card/egg-card.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { EggDataService } from '../services/egg-data.service';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ConfigDataService } from '../services/config-data.service';
+
+@Component({
+  selector: 'app-egg-card',
+  templateUrl: './egg-card.component.html',
+  styleUrls: ['./egg-card.component.scss']
+})
+export class EggCardComponent implements OnInit {
+  public egg;
+  public jsonUrl;
+  @Input() eggKey: string;
+
+  constructor(private eggService: EggDataService, private configService: ConfigDataService, private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.egg = this.eggService.get(this.eggKey);
+
+    if (this.egg.download_json != null) {
+      this.http.get(this.egg.download_json).subscribe((data: Object) => {
+        let blob = new Blob([JSON.stringify(data)], { type: 'application/json'});
+
+        this.jsonUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
+      })
+    }
+  }
+
+  getCardImage() {
+    if (this.egg.image == null) {
+      if (this.egg.official) {
+        return this.configService.getFallbackImages()['official'];
+      } else {
+        return this.configService.getFallbackImages()['default'];
+      }
+    }
+
+    return this.egg.image;
+  }
+
+  getState() {
+    return this.configService.getStateDisplay(this.egg.state);
+  }
+}
---
+++ src/app/egg-card/egg-card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EggCardComponent } from './egg-card.component';
+
+@NgModule({
+  declarations: [EggCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EggCardModule { }
