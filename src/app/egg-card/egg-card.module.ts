de77311ab0d3aba5f52d8e258b309ed6b23df16b
Matthew Hatcher <matthewh@halfpetal.com>
2019-06-27 04:08:44 -0400

    Removing secondary declaration.

--- src/app/egg-card/egg-card.module.ts
+++ src/app/egg-card/egg-card.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EggCardComponent } from './egg-card.component';
 
 @NgModule({
-  declarations: [EggCardComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
