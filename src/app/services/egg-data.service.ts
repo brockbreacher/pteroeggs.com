e8256e283bb345b20063be78f82e98a9d11fb2bb
Matthew Hatcher <matthewh@halfpetal.com>
2019-06-28 15:08:26 -0400

    a starting point for filters.

--- src/app/services/egg-data.service.ts
+++ src/app/services/egg-data.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ConfigDataService } from './config-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EggDataService {
+  private originalEggData: Object = {};
   private eggData: Object = {};
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private configService: ConfigDataService) {
     this.preflight();
   }
 
   private async preflight() {
     await this.http.get('./assets/eggs.json').subscribe(data => {
       this.eggData = data;
+      this.originalEggData = this.eggData;
     });
   }
 
@@ -27,5 +30,14 @@ export class EggDataService {
 
   public get(key: string): Object {
     return this.eggData[key];
+  }
+
+  public filterByTag(tag: string): void {
+    this.eggData = this.originalEggData;
+    let availableTags = this.configService.getAvailableTags();
+
+    Object.keys(this.eggData).forEach((eggKey: string) => {
+      console.log(eggKey);
+    });
   }
 }
